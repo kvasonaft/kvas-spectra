279c8d90e396592b9647f7d64e49dcf34103ea8d
kvasonaft <kvasonaft@proton.me>
2025-06-15 14:21:16 +0300

    Финальный вариант

--- folders-rename.py
+++ folders-rename.py
@@ -1,78 +1,42 @@
 import os
 import pandas as pd
 
-base_dir = '/home/kvasonaft/Development/dataset-code'
-names_table = '/home/kvasonaft/Development/kvas-spectra/folders_names.csv'
-
-df = pd.read_csv(names_table, sep = ';')
-
-mode = 4
+mode = 2
 
 if mode == 1:
 
+    base_dir = '/home/kvasonaft/Development/spectra/dataset-code'
+    names_table = '/home/kvasonaft/Development/names.csv'
+    df = pd.read_csv(names_table, sep = ';')
+
     for _, row in df.iterrows():
-        old_path = os.path.join(base_dir, row['old_name'])
-        new_path = os.path.join(base_dir, str(row['new_name']))
+        old_path = os.path.join(base_dir, row['control_name'])
+        new_path = os.path.join(base_dir, str(row['code_name']))
 
         try:
             if os.path.exists(old_path):
                 os.rename(old_path, new_path)
-                print(f'Папка {row['old_name']} переименована в {row['new_name']}')
+                print(f'Папка {row['control_name']} переименована в {row['code_name']}')
             else:
-                print(f'Папка {row['old_name']} не найдена')
+                print(f'Папка {row['control_name']} не найдена')
         except Exception as e:
             print(f'Ошибка при переименовании {old_path} -> {new_path}: {e}')
 
 elif mode == 2:
 
-    for _, row in df.iterrows():
-        old_path = os.path.join(base_dir, str(row['code_name']))
-        new_path = os.path.join(base_dir, row['code_name'])
-
-        try:
-            if os.path.exists(old_path):
-                os.rename(old_path, new_path)
-                print(f'Папка {row['old_name']} переименована в {row['code_name']}')
-            else:
-                print(f'Папка {row['old_name']} не найдена')
-        except Exception as e:
-            print(f'Ошибка при переименовании {old_path} -> {new_path}: {e}')
-
-elif mode == 3:
-
-    control_path = '/home/kvasonaft/Development/spectra/controls'
+    control_path = '/home/kvasonaft/Development/spectra/dataset-code'
 
     for folder in os.listdir(control_path):
         folder_path = os.path.join(control_path, folder)
         if not os.path.isdir(folder_path):
             continue
 
-        files = os.listdir(folder_path)
+        #files = [file for file in sorted(os.listdir(folder_path)) if os.path.isfile(os.path.join(folder_path, file)) and len(file) > 8]
+
+        files = os.path.listdir(folder_path)
+
         for number, file in enumerate(files, start = 1):
-            old_path = os.path.join(folder_path, file)
 
-            if not os.path.isfile(old_path):
-                continue
-
-            new_name = f'{folder}_{number:02d}.txt'
-            new_path = os.path.join(folder_path, new_name)
-            try:
-                os.rename(old_path, new_path)
-                print(f'Файл {file} переименован в {new_name}')
-            except Exception as e:
-                print(f'Ошибка при переименовании {old_path} -> {new_path}: {e}')
-
-elif mode == 4:
-
-    main_path = '/home/kvasonaft/Development/spectra/dataset-code'
-
-    for folder in os.listdir(main_path):
-        folder_path = os.path.join(main_path, folder)
-        if not os.path.isdir(folder_path):
-            continue
-
-        files = os.listdir(folder_path)
-        for number, file in enumerate(files, start = 1):
             old_path = os.path.join(folder_path, file)
 
             if not os.path.isfile(old_path):
