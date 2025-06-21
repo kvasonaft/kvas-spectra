447ba4b44a2d4afa1b91b4503865f2857cffbbb0
kvasonaft <kvasonaft@proton.me>
2025-06-21 23:06:54 +0300

    Добавление генерации базы данных SQLite

--- folders-rename.py
+++ folders-rename.py
@@ -1,22 +1,22 @@
 import os
 import pandas as pd
 
-mode = 1
+mode = 2
 
 if mode == 1:
 
-    first_path = 'path'
-    names_table = 'path'
+    first_path = '/home/kvasonaft/Development/spectra/dataset'
+    names_table = '/home/kvasonaft/Development/spectra/names.csv'
     df = pd.read_csv(names_table, sep = ';')
 
     for _, row in df.iterrows():
         old_path = os.path.join(first_path, row['control_name'])
-        new_path = os.path.join(first_path, str(row['code_name']))
+        new_path = os.path.join(first_path, str(row['new_name']))
 
         try:
             if os.path.exists(old_path):
                 os.rename(old_path, new_path)
-                print(f'Папка {row['control_name']} переименована в {row['code_name']}')
+                print(f'Папка {row['control_name']} переименована в {row['new_name']}')
             else:
                 print(f'Папка {row['control_name']} не найдена')
         except Exception as e:
@@ -24,16 +24,16 @@ if mode == 1:
 
 elif mode == 2:
 
-    second_path = 'path'
+    second_path = '/home/kvasonaft/Development/spectra/dataset'
 
     for folder in os.listdir(second_path):
         folder_path = os.path.join(second_path, folder)
         if not os.path.isdir(folder_path):
             continue
 
-        #files = [file for file in sorted(os.listdir(folder_path)) if os.path.isfile(os.path.join(folder_path, file)) and len(file) > 8]
+        files = [file for file in sorted(os.listdir(folder_path)) if os.path.isfile(os.path.join(folder_path, file)) and len(file) > 8]
 
-        files = os.path.listdir(folder_path)
+        #files = os.path.listdir(folder_path)
 
         for number, file in enumerate(files, start = 1):
 
