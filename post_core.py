4a7c31cbf6c3a6e5fbcd414332f1e6b53eec9be6
kvasonaft <kvasonaft@Kvasonafts-MacBook-Air.local>
2025-11-07 20:50:21 +0300

    Автоматическое создание таблицы с метками кластеров

--- post_core.py
+++ post_core.py
@@ -5,140 +5,155 @@ from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 from adjustText import adjust_text
-from scipy.cluster.hierarchy import linkage, dendrogram
-from tqdm import tqdm
+from scipy.cluster.hierarchy import linkage, dendrogram, fcluster
 
-area = pd.read_csv('data/area_main.csv', index_col = 'Wavelength')
-peaks = pd.read_csv('data/peaks_main.csv', index_col = 'Wavelength')
+def post_core(circle=False, pca_choise=False, cluster=True):
 
-area = area.dropna()
-peaks = peaks.dropna()
+    area = pd.read_csv('data/area_main.csv', index_col = 'Wavelength')
+    peaks = pd.read_csv('data/peaks_main.csv', index_col = 'Wavelength')
 
-for  idx, table in tqdm(enumerate([area, peaks]), desc = 'Обработка таблиц'):
+    area = area.dropna()
+    peaks = peaks.dropna()
 
-    table = table.T
+    for idx, table in enumerate([area, peaks]):
 
-    scaler = StandardScaler()
-    table_scaled = scaler.fit_transform(table)
+        table = table.T
 
-    kmeans = KMeans(n_clusters = 2, random_state = 7)
-    kmeans.fit(table_scaled)
+        # correlation circle
 
-    pca = PCA(n_components = 2)
-    data_pca = pca.fit_transform(table_scaled)
-    pca_df = pd.DataFrame(data_pca, columns = ['PC1', 'PC2'], index = table.index)
-    pca_df['cluster'] = kmeans.labels_
+        if circle:
 
-    loadings = pca.components_.T * np.sqrt(pca.explained_variance_)
+            scaler = StandardScaler()
+            table_scaled = scaler.fit_transform(table)
 
-    plt.figure(figsize=(8, 8))
-    plt.axhline(0, color='gray', lw=1)
-    plt.axvline(0, color='gray', lw=1)
+            kmeans = KMeans(n_clusters = 2, random_state = 7)
+            kmeans.fit(table_scaled)
 
-    circle = plt.Circle((0,0), 1, color='black', fill=False)
-    plt.gca().add_artist(circle)
+            pca = PCA(n_components = 2)
+            data_pca = pca.fit_transform(table_scaled)
+            pca_df = pd.DataFrame(data_pca, columns = ['PC1', 'PC2'], index = table.index)
+            pca_df['cluster'] = kmeans.labels_
 
-    for i, var in enumerate(table.columns):
-        plt.arrow(0, 0, loadings[i,0], loadings[i,1],
-                color='b', alpha=0.7, head_width=0.03)
-        plt.text(loadings[i,0]*1.1, loadings[i,1]*1.1, var,
-                color='r', ha='center', va='center')
+            loadings = pca.components_.T * np.sqrt(pca.explained_variance_)
 
-    plt.xlabel(f"1-я главная компонента ({pca.explained_variance_ratio_[0]*100:.1f}% дисперсии)")
-    plt.ylabel(f"2-я главная компонента ({pca.explained_variance_ratio_[1]*100:.1f}% дисперсии)")
+            plt.figure(figsize=(8, 8))
+            plt.axhline(0, color='gray', lw=1)
+            plt.axvline(0, color='gray', lw=1)
 
-    if idx == 0:
-        plt.title("Корреляционный круг переменных (по площадям пиков)")
-    else:
-        plt.title("Корреляционный круг переменных (по значениям пиков)")
+            circle = plt.Circle((0,0), 1, color='black', fill=False)
+            plt.gca().add_artist(circle)
 
-    plt.xlim(-1.1, 1.1)
-    plt.ylim(-1.1, 1.1)
-    plt.grid(True)
+            for i, var in enumerate(table.columns):
+                plt.arrow(0, 0, loadings[i,0], loadings[i,1],
+                        color='b', alpha=0.7, head_width=0.03)
+                plt.text(loadings[i,0]*1.1, loadings[i,1]*1.1, var,
+                        color='r', ha='center', va='center')
 
-    if idx == 1:
-        plt.savefig('/Users/kvasonaft/Desktop/Development/kvas-spectra/graphs/diagrams/circle_by_peaks.png', dpi = 600, bbox_inches = 'tight')
-    else:
-        plt.savefig('/Users/kvasonaft/Desktop/Development/kvas-spectra/graphs/diagrams/circle_by_area.png', dpi = 600, bbox_inches = 'tight')
+            plt.xlabel(f"1-я главная компонента ({pca.explained_variance_ratio_[0]*100:.1f}% дисперсии)")
+            plt.ylabel(f"2-я главная компонента ({pca.explained_variance_ratio_[1]*100:.1f}% дисперсии)")
 
-    plt.close()
+            if idx == 0:
+                plt.title("Корреляционный круг переменных (по площадям пиков)")
+            else:
+                plt.title("Корреляционный круг переменных (по значениям пиков)")
 
-    texts = []
+            plt.xlim(-1.1, 1.1)
+            plt.ylim(-1.1, 1.1)
+            plt.grid(True)
 
-    if idx == 1:
-        colors = ['green', 'orange']
-    else:
-        colors = ['orange', 'green']
+            if idx == 1:
+                plt.savefig('graphs/diagrams/circle_by_peaks.png', dpi = 600, bbox_inches = 'tight')
+            else:
+                plt.savefig('graphs/diagrams/circle_by_area.png', dpi = 600, bbox_inches = 'tight')
 
-    fig, ax = plt.subplots(figsize=(12, 8))
+            plt.close()
 
-    for cluster in [0, 1]:
-        ax.scatter(pca_df[pca_df['cluster'] == cluster]['PC1'],
-                    pca_df[pca_df['cluster'] == cluster]['PC2'], color = colors[cluster])
+        # PCA
 
-    for i, (name, row) in enumerate(pca_df.iterrows()):
-        texts.append(
-            ax.text(row['PC1'], row['PC2'], str(i), fontsize=9)
-        )
+        if pca_choise:
 
-    adjust_text(texts)
-    
-    if idx == 1:
-        plt.title(f'Диаграмма двух главных компонент с кластерами, полученными методом k-средних (по значениям пиков)', fontsize = 14)
-    else:
-        plt.title(f'Диаграмма двух главных компонент с кластерами, полученными методом k-средних (по площадям пиков)', fontsize = 14)
+            texts = []
 
-    plt.xlabel(f'Первая главная компонента ({pca.explained_variance_ratio_[0] * 100:.2f}%)', fontsize = 12)
-    plt.ylabel(f'Вторая главная компонента ({pca.explained_variance_ratio_[1] * 100:.2f}%)', fontsize = 12)
+            if idx == 1:
+                colors = ['green', 'orange']
+            else:
+                colors = ['orange', 'green']
 
-    if idx == 1:
-        plt.savefig('/Users/kvasonaft/Desktop/Development/kvas-spectra/graphs/diagrams/pca_by_peaks.png', dpi = 600, bbox_inches = 'tight')
-    else:
-        plt.savefig('/Users/kvasonaft/Desktop/Development/kvas-spectra/graphs/diagrams/pca_by_area.png', dpi = 600, bbox_inches = 'tight')
+            fig, ax = plt.subplots(figsize=(12, 8))
 
-    plt.close()
+            for cluster in [0, 1]:
+                ax.scatter(pca_df[pca_df['cluster'] == cluster]['PC1'],
+                            pca_df[pca_df['cluster'] == cluster]['PC2'], color = colors[cluster])
 
-    linked = linkage(table, method = 'ward', metric = 'euclidean')
-    plt.figure(figsize = (15, 10))
+            for i, (name, row) in enumerate(pca_df.iterrows()):
+                texts.append(
+                    ax.text(row['PC1'], row['PC2'], str(i), fontsize=9)
+                )
 
-    if idx == 1:
-        dendro = dendrogram(linked, labels = table.index.to_list(), leaf_font_size = 10, color_threshold = 0.04, orientation='left')
+            adjust_text(texts)
+            
+            if idx == 1:
+                plt.title(f'Диаграмма двух главных компонент с кластерами, полученными методом k-средних (по значениям пиков)', fontsize = 14)
+            else:
+                plt.title(f'Диаграмма двух главных компонент с кластерами, полученными методом k-средних (по площадям пиков)', fontsize = 14)
 
-        labels = dendro['ivl']
-        indices = dendro['leaves']
+            plt.xlabel(f'Первая главная компонента ({pca.explained_variance_ratio_[0] * 100:.2f}%)', fontsize = 12)
+            plt.ylabel(f'Вторая главная компонента ({pca.explained_variance_ratio_[1] * 100:.2f}%)', fontsize = 12)
 
-        df_dendro = pd.DataFrame({
-            'leaf_index': indices,
-            'label': labels
-        })
+            if idx == 1:
+                plt.savefig('graphs/diagrams/pca_by_peaks.png', dpi = 600, bbox_inches = 'tight')
+            else:
+                plt.savefig('graphs/diagrams/pca_by_area.png', dpi = 600, bbox_inches = 'tight')
 
-        df_dendro.to_csv('dendrogram_peaks.csv')
+            plt.close()
 
-    else:
-        dendro_area = dendrogram(linked, labels = table.index.to_list(), leaf_font_size = 10, color_threshold = 1.4, orientation='left')
+        # hca
 
-        labels = dendro_area['ivl']
-        indices = dendro_area['leaves']
+        if cluster:
 
-        df_dendro_area = pd.DataFrame({
-            'leaf_index': indices,
-            'label': labels
-        })
+            linked = linkage(table, method='ward', metric='euclidean')
+            plt.figure(figsize=(15, 10))
 
-        df_dendro_area.to_csv('dendrogram_area.csv')
+            # задаём разные пороги для разных таблиц
+            if idx == 1:
+                color_threshold = 0.04
+            else:
+                color_threshold = 0.7
 
-    if idx == 1:
-        plt.title('Результаты иерархического кластерного анализа (по значениям пиков)', fontsize = 22)
-    else:
-        plt.title('Результаты иерархического кластерного анализа (по площадям пиков)', fontsize = 22)        
+            dendro = dendrogram(
+                linked,
+                labels=table.index.to_list(),
+                leaf_font_size=10,
+                color_threshold=color_threshold,
+                orientation='left'
+            )
 
-    plt.xlabel('Объекты', fontsize = 18)
-    plt.ylabel('Расстояние', fontsize = 18)
-    plt.tight_layout()
+            # создаём DataFrame с кластерами
 
-    if idx == 1:
-        plt.savefig('/Users/kvasonaft/Desktop/Development/kvas-spectra/graphs/diagrams/dendrogram_by_peaks.png', dpi = 600, bbox_inches = 'tight')
-    else:
-        plt.savefig('/Users/kvasonaft/Desktop/Development/kvas-spectra/graphs/diagrams/dendrogram_by_area.png', dpi = 600, bbox_inches = 'tight')
+            cluster_labels = fcluster(linked, t=color_threshold, criterion='distance')
+            df_clusters = pd.DataFrame({
+                'Sample': table.index,
+                'Cluster': cluster_labels
+            }).set_index('Sample')
 
-    plt.close()
+            # сортировка по номеру кластера
+            df_clusters = df_clusters.sort_values(by='Cluster')
+
+            # сохраняем результаты
+            if idx == 1:
+                df_clusters.to_csv('data/cluster_labels_peaks.csv')
+                plt.title('Результаты иерархического кластерного анализа (по значениям пиков)', fontsize=22)
+                plt.savefig('graphs/diagrams/dendrogram_by_peaks.png', dpi=600, bbox_inches='tight')
+            else:
+                df_clusters.to_csv('data/cluster_labels_area.csv')
+                plt.title('Результаты иерархического кластерного анализа (по площадям пиков)', fontsize=22)
+                plt.savefig('graphs/diagrams/dendrogram_by_area.png', dpi=600, bbox_inches='tight')
+
+            plt.xlabel('Расстояние', fontsize=18)
+            plt.ylabel('Объекты', fontsize=18)
+            plt.tight_layout()
+            plt.close()
+
+if __name__ == '__main__':
+
+    post_core(circle=False, pca_choise=False, cluster=True)
