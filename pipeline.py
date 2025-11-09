a0f28fccf2f02bea16926b0a81af321f0c9874ec
kvasonaft <kvasonaft@Kvasonafts-MacBook-Air.local>
2025-11-10 00:11:46 +0300

    Добавление автоматической кластеризации

--- pipeline.py
+++ pipeline.py
@@ -4,13 +4,14 @@ from renaming import folders_rename, files_rename
 from query import query
 from core_2 import core
 from post_core import post_core
-import counter
+from counter import counter
 from clipper import clipper
 from pathlib import Path
 import shutil
-from regression import regression
+# from regression import regression
 from peaks_and_square import peaks_and_square
-from variance import compute_variance
+# from variance import compute_variance
+import pandas as pd
 
 
 def pipeline(table_path = '/Users/kvasonaft/Desktop/Development/kvas-spectra/data/metadata.xlsx', 
@@ -22,7 +23,7 @@ def pipeline(table_path = '/Users/kvasonaft/Desktop/Development/kvas-spectra/dat
              rename_folders = False,
              renaiming_mode = 'experiments', 
              target = None, 
-           drop_culture = [], cluster_processing=False, cluster_names=None, clusters=None,
+           drop_culture = [], cluster_processing=False,
            ranges=None, csv_separator=';', indicators_on_pictures=False, delta=20, color_con='black',
            color_exp='orange', plot=True, square=True, baseline_square='horizontal_full', savgol_window=21, 
            circle=False, pca_choise=False, cluster=True, json=False, sql=False, compute_differences=True, 
@@ -51,7 +52,7 @@ def pipeline(table_path = '/Users/kvasonaft/Desktop/Development/kvas-spectra/dat
         
         core(target=target, drop_culture=drop_culture, indicators_on_pictures=indicators_on_pictures, 
                 delta=delta, color_con=color_con, color_exp=color_exp, plot=plot, square=square, 
-                baseline_square=baseline_square, savgol_window=savgol_window)
+                baseline_square_ext=baseline_square, savgol_window=savgol_window)
 
     if compute_peaks_and_square:
 
@@ -69,28 +70,67 @@ def pipeline(table_path = '/Users/kvasonaft/Desktop/Development/kvas-spectra/dat
 
         post_core(circle=circle, pca_choise=pca_choise, cluster=cluster)
 
-    if regression_choise:
+    # if regression_choise:
 
-        regression(scatter_color='orange', line_color='green')
+    #     regression(scatter_color='orange', line_color='green')
 
-    if compute_variance_choise:
+    # if compute_variance_choise:
          
-         compute_variance(target=target, mode=variance_mode, filename=variance_filename)
+    #      compute_variance(target=target, mode=variance_mode, filename=variance_filename)
 
     if cluster_processing:
 
-        if cluster_names == None:
-            raise ValueError('Для анализа кластеров введите их названия.')
-        if clusters == None:
-            raise ValueError('Для анализа кластеров введите их список.')
         if ranges == None:
             raise ValueError('Для анализа кластеров введите смысловые диапазоны волновых чисел.')
         
-        for name, cluster_it in zip(cluster_names, clusters):
-            clipper(cluster_name=name, cultures=cluster_it, target=target, ranges=ranges, con_color=color_con, exp_color=color_exp)
-            counter.counter(cluster_name=name, cultures=cluster_it)
+        data_area = pd.read_csv('data/cluster_labels_area.csv')
+        data_area['Cluster'] = data_area['Cluster'].astype(int)
+        cluster_max = max(data_area['Cluster'])
+        cluster_dict = {}
+
+        for i in range(1, cluster_max+1, 1):
+            cluster_dict[i] = []
+
+        for _, row in data_area.iterrows():
+            for i in range(1, cluster_max+1, 1):
+                if row['Cluster'] == i:
+                    if row['Sample'] not in cluster_dict[row['Cluster']]:
+                        cluster_dict[row['Cluster']].append(row['Sample'])
+        clusters = pd.DataFrame(dict([(k, pd.Series(v)) for k, v in cluster_dict.items()]))
+
+        for cluster_num, cluster_data in clusters.items():
+            non_nan_values = cluster_data.dropna()
+            if not non_nan_values.empty:
+                cultures = non_nan_values.tolist()
+                clipper(cluster_name=cluster_num, cultures=cultures, target=target, 
+                    ranges=ranges, con_color=color_con, exp_color=color_exp, added_path=' (clusters by area)')
+                counter(cluster_name=cluster_num, cultures=cultures, area=True, peaks=False)
         
-    shutil.copytree('Graphs', Path(output_path, 'Kvas-spectra results'))
+        data_peaks = pd.read_csv('data/cluster_labels_peaks.csv')
+        data_peaks['Cluster'] = data_peaks['Cluster'].astype(int)
+        cluster_max_peaks = max(data_peaks['Cluster'])
+        cluster_dict_peaks = {}
+
+        for i in range(1, cluster_max_peaks+1, 1):
+            cluster_dict_peaks[i] = []
+
+        for _, row in data_peaks.iterrows():
+            for i in range(1, cluster_max_peaks+1, 1):
+                if row['Cluster'] == i:
+                    if row['Sample'] not in cluster_dict_peaks[row['Cluster']]:
+                        cluster_dict_peaks[row['Cluster']].append(row['Sample'])
+
+        clusters_peaks = pd.DataFrame(dict([(k, pd.Series(v)) for k, v in cluster_dict_peaks.items()]))
+
+        for cluster_num, cluster_data in clusters_peaks.items():
+            non_nan_values = cluster_data.dropna()
+            if not non_nan_values.empty:
+                cultures = non_nan_values.tolist()
+                clipper(cluster_name=cluster_num, cultures=cultures, target=target, 
+                    ranges=ranges, con_color=color_con, exp_color=color_exp, added_path=' (clusters by peaks)')
+                counter(cluster_name=cluster_num, cultures=cultures, peaks=True, area=False)
+
+    shutil.copytree('Graphs', Path(output_path, 'Actual results'))
 
 if __name__ == '__main__':
 
@@ -101,17 +141,23 @@ if __name__ == '__main__':
                 new_column = 'New_names', 
                 rename_files = False,
                 rename_folders = False,
+                cluster_processing=True,
+                json=False,
+                sql=False,
+                compute_differences=False, 
+                regression_choise=False,
+                compute_peaks_and_square=False, 
+                compute_variance_choise=False,
                 renaiming_mode = 'experiments', 
                 target = [3054, 2969, 2908, 1730, 1644, 1577, 1538, 1504, 1470, 1453,
                 1410, 1370, 1342, 1240, 1176, 1096, 1050, 1016, 972, 872, 848, 792], 
-                drop_culture = [], cluster_processing=True, cluster_names=['A', 'B', 'C'], 
-                clusters=[['P-Mw-PET-2_8rec-bt-433-M', 'P-Mw-PET-1-sf-ab-387-D', 'P-Mw-PET-1-bt-ab-387-D'], 
-                          ['P-Mw-PET-2_8rec-bt-433-M', 'P-Mw-PET-1-sf-ab-387-D', 'P-Mw-PET-1-bt-ab-387-D'], 
-                          ['P-Mw-PET-2_8rec-bt-433-M', 'P-Mw-PET-1-sf-ab-387-D', 'P-Mw-PET-1-bt-ab-387-D']],
+                drop_culture = [], 
                 ranges=[(3020, 3080), (2780, 3100), (1480, 1800), (1310, 1520), (900, 1330), (600, 915)], 
-                csv_separator=';', indicators_on_pictures=False, delta=20, color_con='black',
-                color_exp='orange', plot=True, square=True, baseline_square='horizontal_full', savgol_window=21, 
-                circle=False, pca_choise=False, cluster=True, json=False, sql=False, compute_differences=True, 
-                diagrams=True, regression_choise=False, compute_peaks_and_square=False, 
-                compute_variance_choise=False, variance_mode='cv', variance_filename='variance_hf_', 
+                csv_separator=';', indicators_on_pictures=False, 
+                delta=20, color_con='black',
+                color_exp='orange', plot=True, square=True, 
+                baseline_square='horizontal_full', savgol_window=21, 
+                circle=False, pca_choise=False, cluster=True, 
+                diagrams=True,
+                variance_mode='cv', variance_filename='variance_hf_', 
                 output_path='/Users/kvasonaft/Desktop')
